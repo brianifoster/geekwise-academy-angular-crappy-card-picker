2f19e32a6163266c19c980b060fbe84948bcbbd9
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-05-03 17:16:55 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import ICard from "../interfaces/ICard";
+import Deck from "../classes/Deck";
 
 @Component({
 	selector: "application",
@@ -7,8 +9,26 @@ import { Component, OnInit } from "@angular/core";
 export class AppComponent implements OnInit {
 	// write your component code here; create the properties and methods you need to get the job
 	// done as described in "app.html"; start by importing modules you need such as "./../classes/Deck"
+	public deck: Deck;
+	public lastDrawnCard:ICard;
+	public userHand: ICard[];
+	public deckHand: number = 52;
 
 	public ngOnInit():void {
+		this.deck = new Deck();
+		this.userHand = [];
+	}
 
+	public drawCard() {
+		this.lastDrawnCard = this.deck.drawCard();
+		this.userHand.push(this.lastDrawnCard);
+		this.deckHand = (52 - (this.userHand.length));
+	}
+
+	public returnCard(card:ICard) {
+		this.deck.returnCardToDeck(card);
+		this.userHand.splice(this.userHand.indexOf(card), 1);
+		this.deckHand = (52 - (this.userHand.length));
 	}
 }
+
